489ffd9257258005bbd5d96cea8c538674a7503f
HemanshuTala <hemansutala@gmail.com>
2025-08-02 18:41:54 +0530

    User Connection Added

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -4,6 +4,6 @@ const userController = require('../controllers/userController');
 
 router.post('/generate-code', userController.generateCode);
 router.post('/link-parent', userController.linkParent);
-router.get('/:userId', userController.getUser);
+router.get('/user/:userId', userController.getUser);
 router.get('/parent/:parentId', userController.getParent);
 module.exports = router;
